f00b75fea048aa08979ca6c0788854f47a6461fb
Taranjeet Singh <taranjeetsingh0987654@gmail.com>
2026-07-24 17:34:10 +0530

    Time: 0 ms (100%), Space: 96.6 MB (96.44%) - LeetHub

--- 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.cpp
+++ 2670-find-the-distinct-difference-array/2670-find-the-distinct-difference-array.cpp
@@ -1,21 +1,50 @@
+// class Solution {
+// public:
+//     vector<int> distinctDifferenceArray(vector<int>& nums) {
+//         int n=  nums.size();
+//         vector<int> prefix(n), suffix(n), ans(n);
+//         unordered_set<int> st;
+//         for(int i=0;i<n;i++){
+//             st.insert(nums[i]);
+//             prefix[i]= st.size();
+//         }
+//         st.clear();
+//         for(int i=n-1;i>=0;i--){            
+//             suffix[i]= st.size();
+//             st.insert(nums[i]);
+//         }
+//         for(int i=0;i<n;i++){
+//             ans[i] = prefix[i]- suffix[i];
+//         }
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     vector<int> distinctDifferenceArray(vector<int>& nums) {
-        int n=  nums.size();
-        vector<int> prefix(n), suffix(n), ans(n);
-        unordered_set<int> st;
-        for(int i=0;i<n;i++){
-            st.insert(nums[i]);
-            prefix[i]= st.size();
+        int histo[51] = {0};
+        int leftHisto[51] = {0};
+        int rightDistinct = 0;
+        int leftDistinct = 0;
+        vector<int> diff(nums.size(), 0);
+        // populate histogram
+        for (const int &n : nums) {
+            if (histo[n] == 0)
+                ++rightDistinct;
+            ++histo[n];
         }
-        st.clear();
-        for(int i=n-1;i>=0;i--){            
-            suffix[i]= st.size();
-            st.insert(nums[i]);
+        for (int i = 0; i < nums.size(); ++i) {
+            if (leftHisto[nums[i]] == 0) {
+                ++leftDistinct;
+            }
+            ++leftHisto[nums[i]];
+            --histo[nums[i]];
+            if (histo[nums[i]] == 0) {
+                --rightDistinct;
+            }
+            diff[i] = leftDistinct - rightDistinct;
         }
-        for(int i=0;i<n;i++){
-            ans[i] = prefix[i]- suffix[i];
-        }
-        return ans;
+        return diff;
     }
 };
