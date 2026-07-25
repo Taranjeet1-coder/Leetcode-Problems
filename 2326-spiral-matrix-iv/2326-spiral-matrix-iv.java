2c9b97a376171d8ce8a49052210fc5ccbebe5e21
Taranjeet Singh <taranjeetsingh0987654@gmail.com>
2026-07-25 22:50:20 +0530

    Time: 5 ms (94.75%), Space: 134.2 MB (21.38%) - LeetHub

--- 2326-spiral-matrix-iv/2326-spiral-matrix-iv.java
+++ 2326-spiral-matrix-iv/2326-spiral-matrix-iv.java
@@ -9,36 +9,36 @@
  * }
  */
 class Solution {
-    public int[][] spiralMatrix(int m, int n, ListNode temp) {
-        int [][]arr= new int[m][n];
-        ListNode head= temp;
-        int top=0,left=0;
-        int bottom=m-1, right=n-1;
+    public int[][] spiralMatrix(int m, int n, ListNode head) {
+        int[][] matrix = new int[m][];
         for (int i = 0; i < m; i++) {
-            Arrays.fill(arr[i], -1);
+            matrix[i] = new int [n];
+            Arrays.fill(matrix[i], -1);
         }
-        while(head!= null && top<=bottom && left<=right){
-            for(int i=left;i<=right && head!= null;i++){
-                arr[top][i]= head.val;
-                head= head.next;
+        int top = 0, bottom = m - 1;
+        int left = 0, right = n - 1;
+
+        while(head != null){
+            for(int col = left; col <= right && head != null; col++, head = head.next ){
+                matrix[top][col] = head.val;
             }
             top++;
-            for(int i=top;i<=bottom && head!= null;i++){
-                arr[i][right]= head.val;
-                head= head.next;
+
+            for(int row = top; row<=bottom && head != null; row++, head = head.next){
+                matrix[row][right] = head.val;
             }
             right--;
-            for(int i=right;i>=left && head!= null;i--){
-                arr[bottom][i]= head.val;
-                head= head.next;
+
+            for(int col = right; col>=left && head != null; col--, head = head.next){
+                matrix[bottom][col] = head.val;
             }
             bottom--;
-            for(int i=bottom;i>=top && head!= null;i--){
-                arr[i][left]= head.val;
-                head= head.next;
+
+            for(int row = bottom; row>=top && head != null; row--, head = head.next){
+                matrix[row][left] = head.val;
             }
             left++;
         }
-        return arr;
+        return matrix;
     }
 }
