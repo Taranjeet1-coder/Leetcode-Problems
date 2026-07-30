0a240f4caf0d9080177df61e5c93c4e5813566e5
Taranjeet Singh <taranjeetsingh0987654@gmail.com>
2026-07-30 14:10:55 +0530

    Time: 8 ms (25.9%), Space: 99.9 MB (66.79%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,16 +1,40 @@
+// class Solution {
+// public:
+//     int minCost(string col, vector<int>& time) {
+//         int sum=0;
+//         int maxi= time[0];
+//         for(int i=1;i<col.size();i++){
+//             if(col[i] == col[i-1]){
+//                 sum+= min(maxi, time[i]);
+//                 maxi= max(maxi, time[i]);
+//             } else{
+//                 maxi= time[i];
+//             }
+//         }
+//         return sum;
+//     }
+// };
 class Solution {
 public:
-    int minCost(string col, vector<int>& time) {
-        int sum=0;
-        int maxi= time[0];
-        for(int i=1;i<col.size();i++){
-            if(col[i] == col[i-1]){
-                sum+= min(maxi, time[i]);
-                maxi= max(maxi, time[i]);
-            } else{
-                maxi= time[i];
-            }
+    int minCost(string colors, vector<int>& neededTime) 
+    {
+     int totaltime=0;
+     for(int i=1;i<colors.size();i++) 
+     {
+       if(colors[i-1]==colors[i])
+       {
+        if(neededTime[i]<neededTime[i-1])
+        {
+         totaltime=totaltime+neededTime[i];
+         neededTime[i]=neededTime[i-1];
         }
-        return sum;
+        else
+        {
+         totaltime=totaltime+neededTime[i-1];
+         neededTime[i-1]=neededTime[i];
+        }
+       }
+     }
+     return totaltime;
     }
 };
